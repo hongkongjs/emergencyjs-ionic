8cdcc04f074750971fba4050bea1f99eb34e533e
Redback <redback93@hotmail.com>
2015-08-20 23:58:21 +0800

    Add bot creates new emergency channel Change form validation

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,11 @@
 angular.module('emergencyJs.constants', [])
 
-.constant('slack', {
-  apiBase: "https://slack.com/api/",
-  groupToken: "xoxp-4501575029-4501575033-9276716226-e1d85f"
+.constant('SLACK', {
+  API_BASE: "https://slack.com/api/",
+  GROUP_TOKEN: "xoxp-4501575029-4501575033-9276716226-e1d85f"
+})
+
+.constant('CHANNEL_NAMES', {
+  PREFIXES: ['Black', 'Blue', 'Brown', 'Gray', 'Green', 'Orange', 'Pink', 'Purple', 'Red', 'White', 'Yellow'],
+  ROOTS: ['Basilisk','Bigfoot','BlackDog','BlackEyedBeings','BrayRoadBeast','Centaur','Cerberus','Charybdis','Chimera','Cockatrice','CountDracula','Cyclops','Cynocephalus','Demon','Doppelganger','Dragon','Echidna','Elf','Ghost','Golem','Gorgon','Medusa','Griffin','GrimReaper','Hydra','Imp','Ladon','LochNessMonster','Manticore','Medusa','Mermaids','Minotaur','Mothman','Mutants','NemeanLion','NewJerseyDevil','Ogre','Orthros','Pegasus','Phoenix','Sasquatch','Satyr','Scylla','SeaMonsters','Sea-Goat','Shade','Shapeshifters','Sirens','Sphinx','Thunderbird','Typhon','Unicorn','Vampire','Wendigo','Werewolf','Wraith','Zombie']
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,33 +2,79 @@ angular.module('emergencyJs.controllers', ['emergencyJs.constants'])
 
 // var config = require('./config');
 
-.controller('TeamCtrl', ['$scope', '$http', 'slack', 
-  function($scope, $http, slack) {
-    $http.get(slack.apiBase + 'users.list?token=' + slack.groupToken).then(function(users) {
+.controller('TeamCtrl', ['$scope', '$http', 'SLACK', 
+  function($scope, $http, SLACK) {
+    $http.get(SLACK.API_BASE + 'users.list?token=' + SLACK.GROUP_TOKEN).then(function(users) {
       $scope.members = users.data.members;
     });
   }
 ])
 
-.controller('MemberCtrl', ['$scope', '$http', '$stateParams', 'slack', 
-  function($scope, $http, $stateParams, slack) {
-    $http.get(slack.apiBase + 'users.info?token=' + slack.groupToken + '&user=' + $stateParams.memberId).then(function(member) {
+.controller('MemberCtrl', ['$scope', '$http', '$stateParams', 'SLACK', 
+  function($scope, $http, $stateParams, SLACK) {
+    $http.get(SLACK.API_BASE + 'users.info?token=' + SLACK.GROUP_TOKEN + '&user=' + $stateParams.memberId).then(function(member) {
       $scope.member = member.data;
     });
   }
 ])
 
 
-.controller('ChatsCtrl', ['$scope', '$http', 'slack', 
-  function($scope, $http, slack) {
-    $scope.sendMessage = function(message) {
-      $http.get(slack.apiBase + 'chat.postMessage?token=' + slack.groupToken + '&channel=C07NP7YBG&text=' + message).then(function(member) {
+.controller('ChatsCtrl', ['$scope', '$http', '$q', 'SLACK', 'CHANNEL_NAMES', 
+  function($scope, $http, $q, SLACK, CHANNEL_NAMES) {
+    $scope.sendMessage = function(firstname, lastname, email, message) {
+      //See if the channel exists
+      var newChannel = getChannelName();
 
-        // TODO show modal
-        // TODO clear the fields
-        // TODO redirect somewhere :)
+      createChannel(newChannel).then(function(channelId) {
+        var chatMessage = getChatMessage(firstname, lastname, email, message);
 
+        //Valid channel created
+        $http.get(SLACK.API_BASE + 'chat.postMessage?token=' + SLACK.GROUP_TOKEN + '&channel='+channelId+'&text=' + chatMessage).then(function(member) {
+
+          // TODO show modal
+          // TODO clear the fields
+          // TODO redirect somewhere :)
+
+        });
+
+      }, function(error) {
+        //Invalid channel name, retry
+        $scope.sendMessage(message);  
       });
+    }
+
+    getChatMessage = function(firstname, lastname, email, message) {
+      var result = message;
+      if(email)
+        result = '<'+email+'> ' + result;
+
+      if(lastname)
+        result = lastname + ' ' + result;
+      if(firstname)
+        result = firstname + ' ' + result;
+
+      return result;
+    }
+
+    getChannelName = function() {
+      var channelColour = CHANNEL_NAMES.PREFIXES[Math.floor(Math.random() * CHANNEL_NAMES.PREFIXES.length)];
+      var channelRoot = CHANNEL_NAMES.ROOTS[Math.floor(Math.random() * CHANNEL_NAMES.ROOTS.length)];
+      return 'ejs-'+channelColour+'-'+channelRoot;
+    }
+
+    createChannel = function(channelName) {
+      
+      var deferred = $q.defer();
+
+      $http.get(SLACK.API_BASE + 'channels.create?token=' + SLACK.GROUP_TOKEN + '&name=' + channelName).then(function(results) {
+        if(results.data.error && results.data.error == "name_taken") {
+          deferred.reject('name_taken');
+        } else {
+          deferred.resolve(results.data.channel.id);
+        }
+      });
+
+      return deferred.promise;
     }
   }
 ])
